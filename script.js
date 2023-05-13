010fe772edee75164fbbe151e86140aa610017e6
Igwe0001 <nwobodoemmanuel479@gmail.com>
2023-05-13 16:49:08 +0100

    sub-changes

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ const hamburgerItem = document.querySelectorAll(".hamburger__item");
 const navUl = document.querySelector("ul");
 const title = document.querySelector(".home__link");
 const navLinks = document.querySelector(".nav__links");
+const icon = document.querySelector(".search > i");
 
-
+console.log(icon);
 
 // Navigation
 hamburger.addEventListener("click", () => {
@@ -17,10 +18,15 @@ hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
 
-
 //Sticky header
 
 window.addEventListener("scroll", () => {
   let header = document.querySelector("header");
   header.classList.toggle("sticky", window.scrollY > 0);
+  title.classList.toggle("sticky", window.scrollY > 0);
+  navLinks.classList.toggle("sticky", window.scrollY > 0);
+  icon.classList.toggle("sticky", window.scrollY > 0);
+  hamburgerItem.forEach((e) => {
+    e.classList.toggle("sticky", window.scrollY > 0);
+  });
 });
